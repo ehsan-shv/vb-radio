fccfe9097c2db5e73d2e0b885fd06e3e08ae7ead
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-28 15:30:57 +0330

    add Radio and RadioGroup components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <Radio />
+  <h2>Main Stack:</h2>
+  <RadioGroup v-model="framework" :vertical="true" name="frameworks" :options="frameworks" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Radio from './components/Radio.vue';
+import { defineComponent, ref } from 'vue';
+import RadioGroup from './components/RadioGroup.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Radio,
+    RadioGroup,
+  },
+  setup() {
+    const frameworks = ref([
+      { label: 'Vue', value: 'vue' },
+      { label: 'React', value: 'react' },
+      { label: 'Angular', value: 'angular' },
+      { label: 'Svelte', value: 'svelte' },
+    ]);
+
+    const framework = ref('vue');
+
+    return { frameworks, framework };
   },
 });
 </script>
@@ -19,8 +32,12 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 80px;
+
+  h2 {
+    margin-bottom: 40px;
+  }
 }
 </style>
--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Radio</h1>
+  <div class="vb-radio" :class="{ vertical, rtl }">
+    <input type="radio" :checked="modelValue === value" :value="value" v-bind="$attrs" class="vb-radio__field" />
+    <label @click="onLabelClick" class="vb-radio__label" :class="{ rtl }">{{ label }}</label>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,8 +10,121 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Radio',
-  props: {},
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    value: {
+      type: [String, Number],
+      required: true,
+    },
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+    rtl: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const onLabelClick = () => {
+      emit('update:modelValue', props.value);
+    };
+
+    return { onLabelClick };
+  },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --vb-radio-color-default: #212121;
+  --vb-radio-color-secondary: #9e9e9e;
+  --vb-radio-backgrond-color: #2196f3;
+  --vb-radio-elemen-margin: 20px;
+}
+
+.vb-radio {
+  display: inline-flex;
+
+  &:not(:last-child) {
+    margin: 0 var(--vb-radio-elemen-margin) 0 0;
+  }
+
+  &.rtl {
+    &:not(:last-child) {
+      margin: 0 0 0 var(--vb-radio-elemen-margin);
+    }
+  }
+
+  &.vertical {
+    display: flex;
+
+    &:not(:last-child) {
+      margin: 0 0 var(--vb-radio-elemen-margin) 0;
+    }
+  }
+
+  &__field {
+    display: none !important;
+
+    &:checked + label {
+      &::after {
+        background-color: var(--vb-radio-backgrond-color);
+        border: 1px solid var(--vb-radio-backgrond-color);
+      }
+    }
+  }
+
+  &__label {
+    position: relative;
+    display: inline-flex;
+    height: 20px;
+    padding: 2px 0 0 28px;
+    cursor: pointer;
+    user-select: none;
+    color: var(--vb-radio-color-default);
+    font-size: 16px;
+    &.rtl {
+      text-align: right;
+      padding: 2px 28px 0 0;
+
+      &::before,
+      &::after {
+        left: unset;
+        right: 0;
+      }
+    }
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: calc(20px - 2px);
+      height: calc(20px - 2px);
+      border-radius: 100px;
+    }
+    &::before {
+      border: 1px solid var(--vb-radio-color-secondary);
+    }
+
+    &::after {
+      transform: scale(0.5);
+    }
+  }
+}
+</style>
