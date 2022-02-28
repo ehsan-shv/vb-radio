3713c47ead3550e7ad45935f9f748a29cc54db1d
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-28 12:58:16 +0330

    starting cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Radio />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import Radio from './components/Radio.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    Radio,
   },
 });
 </script>
---
+++ src/components/Radio.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Radio</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Radio',
+  props: {},
+});
+</script>
+
+<style lang="scss"></style>
